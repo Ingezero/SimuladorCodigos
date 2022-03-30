b4a98f2e81e00cd45f2e9c30e91b4ee195512fa5
Ingez <ingezero1@gmail.com>
2022-03-29 23:27:09 -1000

    Se agrega identificador de eventos, asi mimo ya se engloba en funcionas los contenidos de las operaciones. El for de etiquetasGen se le a;adio el valor de c (varCommun) para que siga agregando acumulativo si se decide ingresar mas componentes del mismo tipo y no se resetee a 0 en cada interaccion de agregar.

--- js/script.js
+++ js/script.js
@@ -5,64 +5,60 @@ Este es un creador de codigo para productos que ingresan a un inventario.
 let cLog = console.log;
 let identificadorProducto;
 let cantProductos;
-let arranque = prompt('¿Quieres agregar productos? Escribe Si o No').toUpperCase();
 let vRam = 0;
 let vSsd = 0;
 let vUsb = 0;
 let varComun = 0;
 let arrayComponentes = [];
 
-/*Proceso de obtencion de datos al tipo de producto a agregar y cuantos de ellos*/
-while (arranque != 'NO') {
-    switch (arranque) {
-        case 'SI':
-            identificadorProducto = prompt('Inserta el tipo de producto a añadir de los siguientes: RAM, SSD, USB').toUpperCase();
-            cantProductos = parseInt(prompt('¿Cuantos productos se ingresan a inventario?'));
-            switch (identificadorProducto) {
-                case 'RAM':
-                    varComun = vRam;
-                    etiquetasGen(cantProductos, identificadorProducto, varComun);
-                    vRam = vRam + cantProductos;    /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
-                    break;
-                case 'SSD':
-                    varComun = vSsd;
-                    etiquetasGen(cantProductos, identificadorProducto, varComun);
-                    vSsd = vSsd + cantProductos;    /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
-                    break;
-                case 'USB':
-                    varComun = vUsb;
-                    etiquetasGen(cantProductos, identificadorProducto, varComun);
-                    vUsb = vUsb + cantProductos;    /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
-                    break;
-                default:
-                    alert('Tipo de producto incorrecto, intenta nuevamente');
-            }
+//Obtiene referencia del boton que debe ser presionado
+const btnGenDatos = document.getElementById("btnCrearEtiquetas");
+
+// Evento generador de codigos al presionar el boton crear
+btnGenDatos.addEventListener("click", () => {
+    identificadorProducto = document.getElementById("tipoProd").value;
+    cantProductos = parseInt(document.getElementById("numeProd").value);
+    genEtiquetas();
+})
+
+//Funcion principal que genera todo los codigos
+function genEtiquetas() {
+    switch (identificadorProducto) {
+        case 'RAM':
+            varComun = vRam;
+            etiquetasGen(cantProductos, identificadorProducto, varComun);
+            vRam = vRam + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
+            break;
+        case 'SSD':
+            varComun = vSsd;
+            etiquetasGen(cantProductos, identificadorProducto, varComun);
+            vSsd = vSsd + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
+            break;
+        case 'USB':
+            varComun = vUsb;
+            etiquetasGen(cantProductos, identificadorProducto, varComun);
+            vUsb = vUsb + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
             break;
         default:
-            alert('Opcion incorrecta. Intenta nuevamente.')
-            arranque = prompt('¿Quieres agregar productos? Escribe Si o No').toUpperCase();
-            break;
+            alert('Tipo de producto incorrecto, intenta nuevamente');
     }
 }
-
-//Obtiene el elemento donde imprimira las etiquetas generadas
-let productosPrint = document.querySelector("#productosAgregados")
-
-/*Imprime los datos generados dentro del array*/
-for (let i = 0; i < arrayComponentes.length; i++) {
-    productosPrint.innerHTML += arrayComponentes[i].producto;
-    productosPrint.innerHTML += "<br>";    
-}
-
-let finalDiv = document.createElement("div");
-finalDiv.innerHTML = "<h2>Aqui terminan los codigos generados</h2>";
-finalDiv.className = "tituloAlto1";
-document.body.append(finalDiv);
 
 /*Proceso etiquetador. Se usa el mismo sin importar el producto*/
 function etiquetasGen(a, b, c) {
-    for (i = 1; i <= a; i++) {
-        arrayComponentes.push({producto: b + '000' + (i + c)});
+    arrayComponentes = [];
+    for (i = 1 + c; i <= a + c; i++) {
+        arrayComponentes.push({
+            producto: b + '000' + (i)
+        });
     }
-    arranque = prompt('¿Algo mas por agregar? Escribe Si o No').toUpperCase();
+    
+    //Obtiene el elemento donde imprimira las etiquetas generadas
+    let productosPrint = document.querySelector("#productosAgregados")
+    
+    /*Imprime los datos generados dentro del array*/
+    for (let i = 0; i < arrayComponentes.length; i++) {
+        productosPrint.innerHTML += arrayComponentes[i].producto;
+        productosPrint.innerHTML += "<br>";
+    };
 }
