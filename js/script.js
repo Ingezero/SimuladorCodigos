0c4117ba14bdd43394b857c2a5eb5b1a6cad8a2e
Ingez <ingezero1@gmail.com>
2022-04-05 21:41:49 -1000

    Se agrega JSON para mantener el conteo y seguir la serie de productos, necesito mas practica para plotear los datos anteriores

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ Este es un creador de codigo para productos que ingresan a un inventario.
 -Luis Castro-
 */
 let cLog = console.log;
-let identificadorProducto;
-let cantProductos;
+let identificadorProducto = "";
+let cantProductos = 0;
 let vRam = 0;
 let vSsd = 0;
 let vUsb = 0;
@@ -13,6 +13,22 @@ let arrayComponentes = [];
 
 //Obtiene referencia del boton que debe ser presionado
 const btnGenDatos = document.getElementById("btnCrearEtiquetas");
+
+//Ejecuta si hay datos en el localStorage
+function recargarDatos() {
+    vRam = JSON.parse(localStorage.getItem("prodRAM"));
+    identificadorProducto = "RAM";
+    genEtiquetas();
+    vSsd = JSON.parse(localStorage.getItem("prodSSD"));
+    identificadorProducto = "SSD";
+    genEtiquetas();
+    vUsb = JSON.parse(localStorage.getItem("prodUSB"));
+    identificadorProducto = "USB";
+    genEtiquetas();
+    identificadorProducto = ""
+}
+
+recargarDatos();
 
 // Evento generador de codigos al presionar el boton crear
 btnGenDatos.addEventListener("click", () => {
@@ -28,16 +44,19 @@ function genEtiquetas() {
             varComun = vRam;
             etiquetasGen(cantProductos, identificadorProducto, varComun);
             vRam = vRam + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
+            localStorage.setItem("prodRAM", JSON.stringify(vRam));
             break;
         case 'SSD':
             varComun = vSsd;
             etiquetasGen(cantProductos, identificadorProducto, varComun);
             vSsd = vSsd + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
+            localStorage.setItem("prodSSD", JSON.stringify(vSsd));
             break;
         case 'USB':
             varComun = vUsb;
             etiquetasGen(cantProductos, identificadorProducto, varComun);
             vUsb = vUsb + cantProductos; /*Agrega ya en suma dependiendo de cuantos existan ya creados*/
+            localStorage.setItem("prodUSB", JSON.stringify(vUsb));
             break;
         default:
             alert('Tipo de producto incorrecto, intenta nuevamente');
@@ -53,12 +72,14 @@ function etiquetasGen(a, b, c) {
         });
     }
     
+
     //Obtiene el elemento donde imprimira las etiquetas generadas
     let productosPrint = document.querySelector("#productosAgregados")
-    
+
     /*Imprime los datos generados dentro del array*/
     for (let i = 0; i < arrayComponentes.length; i++) {
         productosPrint.innerHTML += arrayComponentes[i].producto;
         productosPrint.innerHTML += "<br>";
     };
+
 }
